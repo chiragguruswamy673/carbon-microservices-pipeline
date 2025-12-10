85c166d00d0250e1f4d88b46fa892c8916d775f6
chiragguruswamy673 <chiragguruswamy673@gmail.com>
2025-12-10 20:33:43 +0530

    Initial commit: project setup with tests

--- src/test/java/api/ApiContractTest.java
+++ src/test/java/api/ApiContractTest.java
@@ -1,25 +1,21 @@
 package api;
 
 import io.restassured.RestAssured;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 public class ApiContractTest {
-    @BeforeClass
-    public void setup() {
-        String base = System.getProperty("base.url", "http://localhost:8080");
-        RestAssured.baseURI = base + "/api";
-    }
-
     @Test
-    public void healthEndpointReturnsOk() {
+    public void getExampleJson_shouldHaveExpectedFields() {
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
         given()
-                .when().get("/health")
+                .when().get("/posts/1")
                 .then()
                 .statusCode(200)
-                .body("status", equalTo("ok"));
+                .body("userId", greaterThan(0))
+                .body("id", equalTo(1))
+                .body("title", not(isEmptyOrNullString()));
     }
 }
